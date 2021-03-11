650b393eb629964ee74f0027ac8016d44d1af3cb
David Angulo <david.angulo.arias@gmail.com>
2021-03-11 15:45:03 -0600

    Define explicit version for dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 from pycfdi import __version__
 
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
 setup(
     name='pycfdi',
     author='VECO',
     version=__version__,
-    include_package_data=True
+    include_package_data=True,
+    install_requires=install_requires,
 )
