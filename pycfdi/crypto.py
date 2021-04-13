c844c1db33b00ba4718877217795daf2c7c30fda
David Angulo <david.angulo.arias@gmail.com>
2021-04-13 15:31:29 -0500

    Add method to sign xml according to W3C standard

--- pycfdi/crypto.py
+++ pycfdi/crypto.py
@@ -1,11 +1,15 @@
 import base64
 import hashlib
+import pycfdi
+import signxml
+from lxml import etree
 from typing import Union
 from pycfdi import exceptions
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding, utils
 from cryptography.hazmat.primitives import serialization
+from dataclasses import is_dataclass
 
 
 def leer_certificado(cer_bytes: bytes) -> x509.Certificate:
@@ -19,6 +23,37 @@ def leer_llave_privada(key_bytes: bytes, password: str) -> rsa.RSAPrivateKey:
         if 'Incorrect password' in str(e):
             raise exceptions.crypto.IncorrectPasswordError()
         raise e
+
+
+def xmldsig(data, cer: x509.Certificate, private_key: rsa.RSAPrivateKey) -> str:
+    import os.path
+    import OpenSSL
+
+    if is_dataclass(data):
+        data = pycfdi.serialization.serialize(data)
+
+    if os.path.isfile(data):
+        data = etree.parse(data)
+    else:
+        data = etree.fromstring(data)
+
+    signer = signxml.XMLSigner(
+        method=signxml.methods.enveloped,
+        signature_algorithm='rsa-sha1',
+        digest_algorithm='sha1'
+    )
+
+    openssl_cer = OpenSSL.crypto.X509().from_cryptography(cer)
+    element = signer.sign(data, key=private_key, cert=[openssl_cer])
+
+    certificate = next(iter(element.xpath(
+        '//ds:Signature/ds:KeyInfo/ds:X509Data/ds:X509Certificate',
+        namespaces={'ds': 'http://www.w3.org/2000/09/xmldsig#'}
+    )), None)
+
+    certificate.text = certificate.text.replace('\n', '')
+
+    return etree.tostring(element, xml_declaration=True)
 
 
 def sellar(message: Union[bytes, str], private_key: rsa.RSAPrivateKey) -> str:
