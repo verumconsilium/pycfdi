91550445e06c5bc7be1a51369132b8490c295538
David Angulo <david.angulo.arias@gmail.com>
2021-04-26 17:22:56 -0500

    Create own implementation of xmldsig to comply with SAT requirements

--- pycfdi/crypto.py
+++ pycfdi/crypto.py
@@ -1,7 +1,6 @@
 import base64
 import hashlib
 import pycfdi
-import signxml
 from lxml import etree
 from typing import Union
 from pycfdi import exceptions
@@ -27,7 +26,6 @@ def leer_llave_privada(key_bytes: bytes, password: str) -> rsa.RSAPrivateKey:
 
 def xmldsig(data, cer: x509.Certificate, private_key: rsa.RSAPrivateKey) -> bytes:
     import os.path
-    import OpenSSL
 
     if is_dataclass(data):
         data = pycfdi.serialization.serialize(data).encode('utf-8')
@@ -37,35 +35,26 @@ def xmldsig(data, cer: x509.Certificate, private_key: rsa.RSAPrivateKey) -> byte
     else:
         data = etree.fromstring(data)
 
-    signer = signxml.XMLSigner(
-        method=signxml.methods.enveloped,
-        signature_algorithm='rsa-sha1',
-        digest_algorithm='sha1'
-    )
-
-    openssl_cer = OpenSSL.crypto.X509().from_cryptography(cer)
-    element = signer.sign(data, key=private_key, cert=[openssl_cer])
-
-    certificate = next(iter(element.xpath(
-        '//ds:Signature/ds:KeyInfo/ds:X509Data/ds:X509Certificate',
-        namespaces={'ds': 'http://www.w3.org/2000/09/xmldsig#'}
-    )), None)
-
-    certificate.text = certificate.text.replace('\n', '')
+    signer = XMLDSigSigner()
+    element = signer.sign(data, key=private_key, cer=cer)
 
     return etree.tostring(element, xml_declaration=True, encoding='utf-8')
 
 
-def sellar(message: Union[bytes, str], private_key: rsa.RSAPrivateKey) -> str:
+def sellar(
+        message: Union[bytes, str],
+        private_key: rsa.RSAPrivateKey,
+        hash_algo: hashes.HashAlgorithm = hashes.SHA256()) -> str:
     if isinstance(message, str):
         message = message.encode('utf-8')
 
-    digest = hashlib.sha256(message).digest()
+    hashlib_algo = getattr(hashlib, hash_algo.name)
+    digest = hashlib_algo(message).digest()
 
     signed = private_key.sign(
         digest,
         padding.PKCS1v15(),
-        utils.Prehashed(hashes.SHA256())
+        utils.Prehashed(hash_algo)
     )
 
     return base64.b64encode(signed).decode('utf-8')
@@ -116,3 +105,72 @@ def __get_subject_value_for_oid(cer: x509.Certificate, oid: x509.ObjectIdentifie
         raise ValueError(f"Failed to read {attribute} from certificate.")
 
     return attributes[0].value.split('/')[0].strip()
+
+
+class XMLDSigSigner:
+
+    def sign(self, data: etree.Element, key: rsa.RSAPrivateKey, cer: x509.Certificate):
+        import copy
+        data = copy.deepcopy(data)
+
+        digest_source = etree.tostring(data, method='c14n')
+        digest_value = base64.b64encode(hashlib.sha1(digest_source).digest())
+
+        signature = etree.Element('Signature', nsmap={None: 'http://www.w3.org/2000/09/xmldsig#'})
+        signed_info = self._create_signed_info_element(digest_value)
+        signature.append(signed_info)
+
+        data.append(signature)
+
+        signed_info_value = sellar(
+            etree.tostring(signed_info, method='c14n'),
+            private_key=key,
+            hash_algo=hashes.SHA1()
+        )
+
+        etree.SubElement(signature, 'SignatureValue').text = signed_info_value
+
+        key_info = self._create_key_info_element(cer)
+        signature.append(key_info)
+
+        return data
+
+    @staticmethod
+    def _create_signed_info_element(digest_value: bytes):
+        template = f"""
+        <SignedInfo>
+            <CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"/>
+            <SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"/>
+            <Reference URI="">
+                <Transforms>
+                  <Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/>
+                </Transforms>
+                <DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"/>
+                <DigestValue>{digest_value.decode()}</DigestValue>
+            </Reference>
+        </SignedInfo>
+        """
+
+        parser = etree.XMLParser(remove_blank_text=True, remove_comments=True)
+        element = etree.fromstring(template, parser=parser)
+
+        return element
+
+    @staticmethod
+    def _create_key_info_element(cer: x509.Certificate):
+        template = f"""
+        <KeyInfo>
+            <X509Data>
+                <X509IssuerSerial>
+                    <X509IssuerName>{cer.issuer.rfc4514_string()}</X509IssuerName>
+                    <X509SerialNumber>{no_certificado(cer)}</X509SerialNumber>
+                </X509IssuerSerial>
+                <X509Certificate>{certificado_base64(cer)}</X509Certificate>
+            </X509Data>
+        </KeyInfo>
+        """
+
+        parser = etree.XMLParser(remove_blank_text=True, remove_comments=True)
+        element = etree.fromstring(template, parser=parser)
+
+        return element
